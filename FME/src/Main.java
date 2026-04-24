9f8ba9ea37064ca4a38c418e28e7d76c62deba52
vishnu2006965 <vr9259@srmist.edu.in>
2026-04-24 09:47:59 +0530

    UC2 Completed

--- FME/src/Main.java
+++ FME/src/Main.java
@@ -1,27 +1,55 @@
-import java.util.*;
+class Feet {
+    double value;
 
-class QuantityMeasurementApp {
+    Feet(double value) {
+        this.value = value;
+    }
 
-    public boolean compareFeet(double value1, double value2) {
-        return value1 == value2;
+    boolean equals(Feet other) {
+        return this.value == other.value;
     }
 }
 
-public class Main {
+class Inches {
+    double value;
+
+    Inches(double value) {
+        this.value = value;
+    }
+
+    boolean equals(Inches other) {
+        return this.value == other.value;
+    }
+}
+
+class QuantityMeasurementApp {
+
+    public static boolean compareFeet(double v1, double v2) {
+        Feet f1 = new Feet(v1);
+        Feet f2 = new Feet(v2);
+        return f1.equals(f2);
+    }
+
+    public static boolean compareInches(double v1, double v2) {
+        Inches i1 = new Inches(v1);
+        Inches i2 = new Inches(v2);
+        return i1.equals(i2);
+    }
+}
+
+public class main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        double feet1 = 10.0;
+        double feet2 = 10.0;
 
-        System.out.print("Enter first value in feet: ");
-        double value1 = sc.nextDouble();
+        double inch1 = 12.0;
+        double inch2 = 12.0;
 
-        System.out.print("Enter second value in feet: ");
-        double value2 = sc.nextDouble();
+        boolean feetResult = QuantityMeasurementApp.compareFeet(feet1, feet2);
+        boolean inchResult = QuantityMeasurementApp.compareInches(inch1, inch2);
 
-        QuantityMeasurementApp app = new QuantityMeasurementApp();
-
-        boolean result = app.compareFeet(value1, value2);
-
-        System.out.println("Are both values equal? " + result);
+        System.out.println("Feet comparison result: " + feetResult);
+        System.out.println("Inches comparison result: " + inchResult);
     }
 }
