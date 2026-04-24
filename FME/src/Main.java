9e008941597fc5da730f73ed32f9b149c180988b
vishnu2006965 <vr9259@srmist.edu.in>
2026-04-24 10:28:56 +0530

    UC7 Completed

--- FME/src/Main.java
+++ FME/src/Main.java
@@ -33,38 +33,38 @@ class QuantityLength {
         return unit.toFeet(this.value);
     }
 
-    static QuantityLength add(QuantityLength l1, QuantityLength l2) {
+    static QuantityLength add(QuantityLength l1, QuantityLength l2, Unit targetUnit) {
 
         double l1InFeet = l1.toFeet();
         double l2InFeet = l2.toFeet();
 
         double sumInFeet = l1InFeet + l2InFeet;
 
-        double resultValue = l1.unit.fromFeet(sumInFeet);
+        double resultValue = targetUnit.fromFeet(sumInFeet);
 
-        return new QuantityLength(resultValue, l1.unit);
+        return new QuantityLength(resultValue, targetUnit);
     }
 }
 
 public class main {
     public static void main(String[] args) {
 
-        System.out.println("=== UC6 Addition of Lengths ===");
+        System.out.println("=== UC7 Addition with Target Unit ===");
 
         QuantityLength length1 = new QuantityLength(1, Unit.FEET);
         QuantityLength length2 = new QuantityLength(12, Unit.INCH);
 
-        QuantityLength result1 = QuantityLength.add(length1, length2);
+        QuantityLength result1 = QuantityLength.add(length1, length2, Unit.YARD);
 
-        System.out.println("1 Feet + 12 Inch = " +
+        System.out.println("1 Feet + 12 Inch in Yards = " +
                 result1.value + " " + result1.unit);
 
         QuantityLength length3 = new QuantityLength(2, Unit.YARD);
         QuantityLength length4 = new QuantityLength(36, Unit.INCH);
 
-        QuantityLength result2 = QuantityLength.add(length3, length4);
+        QuantityLength result2 = QuantityLength.add(length3, length4, Unit.FEET);
 
-        System.out.println("2 Yard + 36 Inch = " +
+        System.out.println("2 Yard + 36 Inch in Feet = " +
                 result2.value + " " + result2.unit);
     }
 }
